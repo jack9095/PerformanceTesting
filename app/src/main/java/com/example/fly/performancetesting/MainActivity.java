0c7b47b27cb1f5da12dd7bdaf70042dbb5b60369
wangfei <gaodun123com>
2018-12-29 16:51:40 +0800

    加入常用的检测性能的链接

--- app/src/main/java/com/example/fly/performancetesting/MainActivity.java
+++ app/src/main/java/com/example/fly/performancetesting/MainActivity.java
@@ -6,26 +6,22 @@ import android.support.v7.app.AppCompatActivity;
 import jp.wasabeef.takt.Takt;
 
 /**
- * Copyright (C) 2017 Wasabeef
+ * https://github.com/android-notes/SwissArmyKnife   UI调试工具
+ * https://github.com/android-notes/Cockroach       降低Android非必要crash
+ * https://github.com/android-notes
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * https://github.com/friendlyrobotnyc/TinyDancer  可以检测 fps 图像每帧的耗时
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://github.com/wasabeef/Takt   可以检测 fps
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * https://blog.csdn.net/wangwwish/article/details/77719715  博客
  */
-
 public class MainActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+//    MyApplication.getInstance().stock.play();
   }
 
   @Override
--- app/src/main/java/com/example/fly/performancetesting/SampleApplication.java
+++ app/src/main/java/com/example/fly/performancetesting/SampleApplication.java
@@ -7,35 +7,29 @@ import jp.wasabeef.takt.Audience;
 import jp.wasabeef.takt.Seat;
 import jp.wasabeef.takt.Takt;
 
-/**
- * Copyright (C) 2017 Wasabeef
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 public class SampleApplication extends Application {
-
+    public Takt.Program stock;
   @Override public void onCreate() {
     super.onCreate();
+
+//      stock = Takt.stock(this)
+//              .seat(Seat.BOTTOM_RIGHT)
+//              .interval(250)
+//              .color(Color.RED)
+//              .size(14f)
+//              .alpha(.5f);
+
+
     Takt.stock(this)
         .seat(Seat.TOP_RIGHT)
-        .interval(250)
+        .interval(500)
         .color(Color.WHITE)
         .size(14f)
         .alpha(.5f)
         .listener(new Audience() {
           @Override public void heartbeat(double fps) {
-            Log.d("Excellent!", fps + " fps");
+            Log.e("Excellent!", fps + " fps");
           }
         })
         .useCustomControl();
